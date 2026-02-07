9fa608b089437172de953f9d85fc1c1395ced81a
hayun524 <2peanutbutter0604@gmail.com>
2026-02-08 00:24:18 +0900

    로그인 화면 사이즈 확인용

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 /** @jsxImportSource @emotion/react */
 
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import styled from '@emotion/styled';
 //import Button from './components/Button/Button';
 //import Input from './components/Input';
+
 import Header from './components/Header/Header';
+import React from 'react';
+import Login from './pages/Login/Login';
+
+const Background = styled.div`
+  width: 100vw; /* 뷰포트 전체 너비 */
+  height: 100vh; /* 뷰포트 전체 높이 */
+  background-color: #f5f5f5; /* 원하는 배경색 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <>
+    <Background>
       <Header />
-    </>
+      <Login />
+    </Background>
   );
 }
 
