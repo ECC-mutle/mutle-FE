4b5e71af2ef9b5686c7ce86d49d46a9a50de2025
LEE <iyean7297@gmail.com>
2026-02-19 14:21:07 +0900

    d

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -87,6 +87,7 @@ export default function Friends() {
               refresh={() => {
                 fetchRequests();
                 fetchFriends();
+                setShowRequestList(false);
               }}
             />
           ) : (
